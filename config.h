6da06b769c7bf4ef91b77570b646373041142d81
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-08-12 23:18:00 -0300

    formatting

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",	"$STATUSBAR_BIN/pacpackages",	0,	3},
-	{"",	"$STATUSBAR_BIN/news",	0,	4},
-	{"",	"$STATUSBAR_BIN/coronavirus",	3600*12,	11},
-	{"",	"$STATUSBAR_BIN/weather",	3600*12,	10},
-	{"",	"$STATUSBAR_BIN/disk /",	0,	12},
-	{"",	"$STATUSBAR_BIN/battery",	0,	12},
-	{"",	"$STATUSBAR_BIN/volume",	0,	2},
-	{"",	"$STATUSBAR_BIN/clock",	60,	1},
+	/*Icon |	Comman                       | Update Interva |	Update Signal */
+	{""    , "$STATUSBAR_BIN/pacpackages"     , 0              , 3},
+	{""    , "$STATUSBAR_BIN/news"            , 0              , 4},
+	{""    , "$STATUSBAR_BIN/coronavirus"     , 3600*12        , 11},
+	{""    , "$STATUSBAR_BIN/weather"         , 3600*12        , 10},
+	{""    , "$STATUSBAR_BIN/disk /"          , 0              , 12},
+	{""    , "$STATUSBAR_BIN/battery"         , 0              , 12},
+	{""    , "$STATUSBAR_BIN/volume"          , 0              , 2},
+	{""    , "$STATUSBAR_BIN/clock"           , 2              , 1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

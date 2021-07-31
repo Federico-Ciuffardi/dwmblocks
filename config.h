79eb0e7747633bebbefe7a7a384e47ca7972bed9
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2021-07-30 21:50:05 -0300

    Change blocks

--- config.h
+++ config.h
@@ -7,14 +7,15 @@ static const Block blocks[] = {
 	{""    , "$STATUSBAR_BIN/sb-membars"      , 1              , 13 },
 	{""    , "$STATUSBAR_BIN/disk /"          , 30             , 18 },
 	/* {""    , "$STATUSBAR_BIN/tmux"         , 0              , 20 }, */
-	{""    , "$STATUSBAR_BIN/sb-nettraf"      , 5              , 16 },
+	{""    , "$STATUSBAR_BIN/sb-nettraf"      , 1              , 16 },
 	{""    , "$STATUSBAR_BIN/weather"         , 3600*1         , 10 },
 	/* {""    , "$STATUSBAR_BIN/coronavirus"  , 3600*12        , 11 }, */
 	/* {""    , "$STATUSBAR_BIN/news"         , 0              , 4  }, */
-	{""    , "$STATUSBAR_BIN/sb-mailbox"      , 3600*1         , 17 },
+	{""    , "$STATUSBAR_BIN/sb-mailbox"      , 60*10          , 17 },
 	{""    , "$STATUSBAR_BIN/pacpackages"     , 0              , 3  },
 	{""    , "$STATUSBAR_BIN/battery"         , 30             , 15 },
 	{""    , "$STATUSBAR_BIN/volume"          , 0              , 2  },
+	{""    , "$STATUSBAR_BIN/timer"           , 1              , 5  },
 	{""    , "$STATUSBAR_BIN/clock"           , 30             , 1  },
 };
 

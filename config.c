dc71c28a490e66b4b6b99ee24dfc1a143998825a
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2025-01-17 13:24:18 -0300

    Remove mail block

--- config.c
+++ config.c
@@ -9,7 +9,7 @@ Block blocks[] = {
     {"$STATUSBAR_BIN/sb-membars"  , 1                , 13            },
     {"$STATUSBAR_BIN/disk /"      , 30               , 18            },
     {"$STATUSBAR_BIN/weather"     , 60*60            , 10            },
-    {"$STATUSBAR_BIN/sb-mailbox"  , 60*10            , 17            },
+    /* {"$STATUSBAR_BIN/sb-mailbox"  , 60*10            , 17            }, */
     {"$STATUSBAR_BIN/pacpackages" , 60*60            , 3             },
     {"$STATUSBAR_BIN/battery"     , 30               , 15            },
     {"$STATUSBAR_BIN/volume"      , 0                , 2             },
